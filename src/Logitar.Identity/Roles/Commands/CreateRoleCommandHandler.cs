d86deeaf56e00d68ebd27116eb63cce96d698952
Francis Pion <francispion@hotmail.com>
2023-03-04 18:32:08 -0500

    Refactored role creation.

--- src/Logitar.Identity/Roles/Commands/CreateRoleCommandHandler.cs
+++ src/Logitar.Identity/Roles/Commands/CreateRoleCommandHandler.cs
@@ -18,6 +18,10 @@ internal class CreateRoleCommandHandler : IRequestHandler<CreateRoleCommand, Rol
   /// </summary>
   private readonly IEventStore _eventStore;
   /// <summary>
+  /// The realm repository.
+  /// </summary>
+  private readonly IRealmRepository _realmRepository;
+  /// <summary>
   /// The role querier.
   /// </summary>
   private readonly IRoleQuerier _roleQuerier;
@@ -31,15 +35,18 @@ internal class CreateRoleCommandHandler : IRequestHandler<CreateRoleCommand, Rol
   /// </summary>
   /// <param name="currentActor">The current actor.</param>
   /// <param name="eventStore">The event store.</param>
+  /// <param name="realmRepository">The realm repository.</param>
   /// <param name="roleQuerier">The role querier.</param>
   /// <param name="roleRepository">The role repository.</param>
   public CreateRoleCommandHandler(ICurrentActor currentActor,
     IEventStore eventStore,
+    IRealmRepository realmRepository,
     IRoleQuerier roleQuerier,
     IRoleRepository roleRepository)
   {
     _currentActor = currentActor;
     _eventStore = eventStore;
+    _realmRepository = realmRepository;
     _roleQuerier = roleQuerier;
     _roleRepository = roleRepository;
   }
@@ -57,9 +64,8 @@ internal class CreateRoleCommandHandler : IRequestHandler<CreateRoleCommand, Rol
   {
     CreateRoleInput input = command.Input;
 
-    AggregateId realmId = new(input.RealmId);
-    RealmAggregate realm = await _eventStore.LoadAsync<RealmAggregate>(realmId, cancellationToken)
-      ?? throw new AggregateNotFoundException<RealmAggregate>(realmId, nameof(input.RealmId));
+    RealmAggregate realm = await _realmRepository.LoadAsync(input.Realm, cancellationToken)
+      ?? throw new AggregateNotFoundException<RealmAggregate>(new AggregateId(input.Realm), nameof(input.Realm));
 
     if (await _roleRepository.LoadAsync(realm, input.UniqueName, cancellationToken) != null)
     {
