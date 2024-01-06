a2f4ac33094389d88626313367c615e0b13285b2
Francis Pion <francispion@hotmail.com>
2024-01-06 12:54:05 -0500

    Implemented unique email addresses.

--- src/Logitar.Identity.EntityFrameworkCore.Relational/IdentityDb.cs
+++ src/Logitar.Identity.EntityFrameworkCore.Relational/IdentityDb.cs
@@ -18,6 +18,7 @@ public static class IdentityDb
     public static readonly TableId Table = new(nameof(IdentityContext.Users));
 
     public static readonly ColumnId AggregateId = new(nameof(UserEntity.AggregateId), Table);
+    public static readonly ColumnId EmailAddressNormalized = new(nameof(UserEntity.EmailAddressNormalized), Table);
     public static readonly ColumnId TenantId = new(nameof(UserEntity.TenantId), Table);
     public static readonly ColumnId UniqueNameNormalized = new(nameof(UserEntity.UniqueNameNormalized), Table);
     public static readonly ColumnId UserId = new(nameof(UserEntity.UserId), Table);
