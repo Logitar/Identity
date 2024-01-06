4bd96ae53691437b12e3a64e96aebe2cab2b5de8
Francis Pion <francispion@hotmail.com>
2024-01-06 13:59:55 -0500

    Implemented JSON converters. (#23)

---
+++ src/Logitar.Identity.Infrastructure/Converters/LocaleConverter.cs
@@ -0,0 +1,17 @@
+﻿using Logitar.Identity.Domain.Shared;
+
+namespace Logitar.Identity.Infrastructure.Converters;
+
+public class LocaleConverter : JsonConverter<LocaleUnit>
+{
+  public override LocaleUnit? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+  {
+    string? code = reader.GetString();
+    return code == null ? null : new(code); // TODO(fpion): shouldn't validate
+  }
+
+  public override void Write(Utf8JsonWriter writer, LocaleUnit locale, JsonSerializerOptions options)
+  {
+    writer.WriteStringValue(locale.Code);
+  }
+}
