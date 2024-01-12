5fd648557c88a0710443a2a581237d634f7fe632
Francis Pion <francispion@hotmail.com>
2024-01-12 14:23:40 -0500

    Added serialization constructors to contact informations.

--- src/Logitar.Identity.Domain/Users/PhoneUnit.cs
+++ src/Logitar.Identity.Domain/Users/PhoneUnit.cs
@@ -40,9 +40,32 @@ public record PhoneUnit : ContactUnit, IPhone
   /// <param name="number">The phone number.</param>
   /// <param name="countryCode">The <see href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">ISO 3166-1 alpha-2 country code</see> of the phone.</param>
   /// <param name="extension">The phone extension.</param>
+  /// <param name="propertyName">The name of the property, used for validation.</param>
+  public PhoneUnit(string number, string? countryCode = null, string? extension = null, string? propertyName = null)
+    : this(number, countryCode, extension, isVerified: false, propertyName)
+  {
+  }
+  /// <summary>
+  /// Initializes a new instance of the <see cref="PhoneUnit"/> class.
+  /// </summary>
+  /// <param name="number">The phone number.</param>
+  /// <param name="countryCode">The <see href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">ISO 3166-1 alpha-2 country code</see> of the phone.</param>
+  /// <param name="extension">The phone extension.</param>
+  /// <param name="isVerified">A value indicating whether or not the phone number is verified.</param>
+  [JsonConstructor]
+  public PhoneUnit(string number, string? countryCode, string? extension, bool isVerified)
+    : this(number, countryCode, extension, isVerified, propertyName: null)
+  {
+  }
+  /// <summary>
+  /// Initializes a new instance of the <see cref="PhoneUnit"/> class.
+  /// </summary>
+  /// <param name="number">The phone number.</param>
+  /// <param name="countryCode">The <see href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">ISO 3166-1 alpha-2 country code</see> of the phone.</param>
+  /// <param name="extension">The phone extension.</param>
   /// <param name="isVerified">A value indicating whether or not the phone number is verified.</param>
   /// <param name="propertyName">The name of the property, used for validation.</param>
-  public PhoneUnit(string number, string? countryCode = null, string? extension = null, bool isVerified = false, string? propertyName = null)
+  public PhoneUnit(string number, string? countryCode, string? extension, bool isVerified, string? propertyName)
     : base(isVerified)
   {
     CountryCode = countryCode?.CleanTrim();
