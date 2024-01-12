928fee94fc0f9a1913c1388e574b0da45ee4bca1
Francis Pion <francispion@hotmail.com>
2024-01-11 20:10:28 -0500

    Added a migration.

--- src/Logitar.Identity.EntityFrameworkCore.SqlServer/Migrations/20240112010955_AlterUserPhoneColumnLengths.Designer.cs
+++ src/Logitar.Identity.EntityFrameworkCore.SqlServer/Migrations/20240112010955_AlterUserPhoneColumnLengths.Designer.cs
@@ -4,6 +4,7 @@ using Logitar.Identity.EntityFrameworkCore.Relational;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Logitar.Identity.EntityFrameworkCore.SqlServer.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20240112010955_AlterUserPhoneColumnLengths")]
+    partial class AlterUserPhoneColumnLengths
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
