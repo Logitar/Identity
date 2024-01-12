c2b2b8d682255e5895061af069b78f87fce7cc4b
Francis Pion <francispion@hotmail.com>
2024-01-12 15:22:05 -0500

    Refactored UserNotFoundException.

--- src/Logitar.Identity.Domain/Users/UserNotFoundException.cs
+++ src/Logitar.Identity.Domain/Users/UserNotFoundException.cs
@@ -1,4 +1,5 @@
-﻿using Logitar.Identity.Domain.Shared;
+﻿using Logitar.Identity.Domain.Settings;
+using Logitar.Identity.Domain.Shared;
 
 namespace Logitar.Identity.Domain.Users;
 
@@ -12,20 +13,25 @@ public class UserNotFoundException : InvalidCredentialsException
   /// </summary>
   public new const string ErrorMessage = "The specified user could not be found.";
 
+  private static readonly UniqueNameSettings _uniqueNameSettings = new()
+  {
+    AllowedCharacters = null // NOTE(fpion): strict validation is not required when deserializing an unique name.
+  };
+
   /// <summary>
   /// Gets or sets the identifier of the tenant in which the user was searched.
   /// </summary>
-  public string? TenantId
+  public TenantId? TenantId
   {
-    get => (string?)Data[nameof(TenantId)];
-    private set => Data[nameof(TenantId)] = value;
+    get => TenantId.TryCreate((string?)Data[nameof(TenantId)]);
+    private set => Data[nameof(TenantId)] = value?.Value;
   }
   /// <summary>
   /// Gets or sets the unique name of the user who was searched.
   /// </summary>
-  public string UniqueName
+  public UniqueNameUnit UniqueName
   {
-    get => (string)Data[nameof(UniqueName)]!;
+    get => new(_uniqueNameSettings, (string)Data[nameof(UniqueName)]!);
     private set => Data[nameof(UniqueName)] = value;
   }
 
@@ -34,14 +40,14 @@ public class UserNotFoundException : InvalidCredentialsException
   /// </summary>
   /// <param name="tenantId">The identifier of the tenant in which the user was searched.</param>
   /// <param name="uniqueName">The unique name of the user who was searched.</param>
-  public UserNotFoundException(string? tenantId, string uniqueName) : base(BuildMessage(tenantId, uniqueName))
+  public UserNotFoundException(TenantId? tenantId, UniqueNameUnit uniqueName) : base(BuildMessage(tenantId, uniqueName))
   {
     TenantId = tenantId;
     UniqueName = uniqueName;
   }
 
-  private static string BuildMessage(string? tenantId, string uniqueName) => new ErrorMessageBuilder(ErrorMessage)
-    .AddData(nameof(TenantId), tenantId, "<null>")
-    .AddData(nameof(UniqueName), uniqueName)
+  private static string BuildMessage(TenantId? tenantId, UniqueNameUnit uniqueName) => new ErrorMessageBuilder(ErrorMessage)
+    .AddData(nameof(TenantId), tenantId?.Value, "<null>")
+    .AddData(nameof(UniqueName), uniqueName.Value)
     .Build();
 }
