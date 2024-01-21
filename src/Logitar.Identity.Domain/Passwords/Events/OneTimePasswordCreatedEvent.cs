b85c6753cf4f66f508e1621aa6b19b7f3cd93fe0
Francis Pion <francispion@hotmail.com>
2024-01-21 15:37:40 -0500

    Implementing One-Time Passwords.

---
+++ src/Logitar.Identity.Domain/Passwords/Events/OneTimePasswordCreatedEvent.cs
@@ -0,0 +1,39 @@
+﻿using Logitar.EventSourcing;
+using MediatR;
+
+namespace Logitar.Identity.Domain.Passwords.Events;
+
+/// <summary>
+/// The event raised when a new One-Time Password (OTP) is created.
+/// </summary>
+public record OneTimePasswordCreatedEvent : DomainEvent, INotification
+{
+  /// <summary>
+  /// Gets the encoded value of the One-Time Password (OTP).
+  /// </summary>
+  public Password Password { get; }
+
+  /// <summary>
+  /// Gets the expiration date and time of the One-Time Password (OTP).
+  /// </summary>
+  public DateTime? ExpiresOn { get; }
+  /// <summary>
+  /// Gets the maximum number of attempts of the One-Time Password (OTP).
+  /// </summary>
+  public int? MaximumAttempts { get; }
+
+  /// <summary>
+  /// Initializes a new instance of the <see cref="OneTimePasswordCreatedEvent"/> class.
+  /// </summary>
+  /// <param name="actorId">The actor identifier.</param>
+  /// <param name="expiresOn">The expiration date and time of the One-Time Password (OTP).</param>
+  /// <param name="maximumAttempts">The maximum number of attempts of the One-Time Password (OTP).</param>
+  /// <param name="password">The encoded value of the One-Time Password (OTP).</param>
+  public OneTimePasswordCreatedEvent(ActorId actorId, DateTime? expiresOn, int? maximumAttempts, Password password)
+  {
+    ActorId = actorId;
+    ExpiresOn = expiresOn;
+    MaximumAttempts = maximumAttempts;
+    Password = password;
+  }
+}
---
+++ src/Logitar.Identity.Domain/Passwords/MaximumAttemptsReachedException.cs
@@ -0,0 +1,48 @@
+﻿using Logitar.Identity.Domain.Shared;
+
+namespace Logitar.Identity.Domain.Passwords;
+
+/// <summary>
+/// The exception raised when the maximum number of attempts has been reached for a One-Time Password (OTP).
+/// </summary>
+public class MaximumAttemptsReachedException : InvalidCredentialsException
+{
+  /// <summary>
+  /// A generic error message for this exception.
+  /// </summary>
+  public new const string ErrorMessage = "The maximum number of attempts has been reached for this One-Time Password (OTP).";
+
+  /// <summary>
+  /// Gets or sets the identifier of the One-Time Password (OTP).
+  /// </summary>
+  public OneTimePasswordId OneTimePasswordId
+  {
+    get => new((string)Data[nameof(OneTimePasswordId)]!);
+    private set => Data[nameof(OneTimePasswordId)] = value.Value;
+  }
+  /// <summary>
+  /// Gets or sets the number of attempts.
+  /// </summary>
+  public int AttemptCount
+  {
+    get => (int)Data[nameof(AttemptCount)]!;
+    private set => Data[nameof(AttemptCount)] = value;
+  }
+
+  /// <summary>
+  /// Initializes a new instance of the <see cref="MaximumAttemptsReachedException"/> class.
+  /// </summary>
+  /// <param name="oneTimePassword">The One-Time Password (OTP).</param>
+  /// <param name="attemptCount">The number of attempts.</param>
+  public MaximumAttemptsReachedException(OneTimePasswordAggregate oneTimePassword, int attemptCount)
+    : base(BuildMessage(oneTimePassword, attemptCount))
+  {
+    AttemptCount = attemptCount;
+    OneTimePasswordId = oneTimePassword.Id;
+  }
+
+  private static string BuildMessage(OneTimePasswordAggregate oneTimePassword, int attemptCount) => new ErrorMessageBuilder(ErrorMessage)
+    .AddData(nameof(OneTimePasswordId), oneTimePassword.Id.Value)
+    .AddData(nameof(AttemptCount), attemptCount)
+    .Build();
+}
