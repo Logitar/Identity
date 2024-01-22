8aa18649293ade5ab25927914ffb25a26a5a2099
Francis Pion <francispion@hotmail.com>
2024-01-21 22:04:08 -0500

    Refactored demo project.

---
+++ src/Logitar.Identity.Demo/Controllers/IndexController.cs
@@ -0,0 +1,13 @@
+﻿using Logitar.Identity.Demo.Models.Index;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logitar.Identity.Demo.Controllers;
+
+[ApiController]
+[ApiExplorerSettings(IgnoreApi = true)]
+[Route("")]
+public class IndexController : ControllerBase
+{
+  [HttpGet]
+  public ActionResult<ApiVersion> Get() => Ok(new ApiVersion());
+}
