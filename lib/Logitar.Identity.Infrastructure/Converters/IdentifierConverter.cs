0fc35df483e8f53e349ad77b21860a0fda823a21
Francis Pion <francispion@hotmail.com>
2024-12-20 19:26:46 -0500

    fix

--- lib/Logitar.Identity.Infrastructure/Converters/IdentifierConverter.cs
+++ lib/Logitar.Identity.Infrastructure/Converters/IdentifierConverter.cs
@@ -1,4 +1,5 @@
 ﻿using Logitar.Identity.Core;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -11,8 +12,18 @@ public class IdentifierConverter : JsonConverter<Identifier>
     return Identifier.TryCreate(reader.GetString());
   }
 
+  public override Identifier ReadAsPropertyName(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+  {
+    return Read(ref reader, typeToConvert, options) ?? throw new InvalidOperationException("The identifier could not be read.");
+  }
+
   public override void Write(Utf8JsonWriter writer, Identifier identifier, JsonSerializerOptions options)
   {
     writer.WriteStringValue(identifier.Value);
+  }
+
+  public override void WriteAsPropertyName(Utf8JsonWriter writer, [DisallowNull] Identifier identifier, JsonSerializerOptions options)
+  {
+    writer.WritePropertyName(identifier.Value);
   }
 }
