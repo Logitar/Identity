67e7072bc73c7ab1cbd72a2689efbaf807997686
Francis Pion <francispion@hotmail.com>
2024-12-23 00:04:41 -0500

    SqlServer

---
+++ lib/Logitar.Identity.EntityFrameworkCore.SqlServer/Scripts/20241223050308_Release_3_0_0.sql
@@ -0,0 +1,431 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+IF SCHEMA_ID(N'Identity') IS NULL EXEC(N'CREATE SCHEMA [Identity];');
+
+CREATE TABLE [Identity].[Actors] (
+    [ActorId] int NOT NULL IDENTITY,
+    [Id] nvarchar(255) NOT NULL,
+    [Type] nvarchar(255) NOT NULL,
+    [IsDeleted] bit NOT NULL,
+    [DisplayName] nvarchar(255) NOT NULL,
+    [EmailAddress] nvarchar(255) NULL,
+    [PictureUrl] nvarchar(2048) NULL,
+    CONSTRAINT [PK_Actors] PRIMARY KEY ([ActorId])
+);
+
+CREATE TABLE [Identity].[ApiKeys] (
+    [ApiKeyId] int NOT NULL IDENTITY,
+    [TenantId] nvarchar(255) NULL,
+    [EntityId] nvarchar(255) NOT NULL,
+    [SecretHash] nvarchar(255) NOT NULL,
+    [DisplayName] nvarchar(255) NOT NULL,
+    [Description] nvarchar(max) NULL,
+    [ExpiresOn] datetime2 NULL,
+    [AuthenticatedOn] datetime2 NULL,
+    [CustomAttributes] nvarchar(max) NULL,
+    [StreamId] nvarchar(255) NOT NULL,
+    [Version] bigint NOT NULL,
+    [CreatedBy] nvarchar(255) NULL,
+    [CreatedOn] datetime2 NOT NULL,
+    [UpdatedBy] nvarchar(255) NULL,
+    [UpdatedOn] datetime2 NOT NULL,
+    CONSTRAINT [PK_ApiKeys] PRIMARY KEY ([ApiKeyId])
+);
+
+CREATE TABLE [Identity].[CustomAttributes] (
+    [CustomAttributeId] int NOT NULL IDENTITY,
+    [EntityType] nvarchar(255) NOT NULL,
+    [EntityId] int NOT NULL,
+    [Key] nvarchar(255) NOT NULL,
+    [Value] nvarchar(max) NOT NULL,
+    [ValueShortened] nvarchar(255) NOT NULL,
+    CONSTRAINT [PK_CustomAttributes] PRIMARY KEY ([CustomAttributeId])
+);
+
+CREATE TABLE [Identity].[OneTimePasswords] (
+    [OneTimePasswordId] int NOT NULL IDENTITY,
+    [TenantId] nvarchar(255) NULL,
+    [EntityId] nvarchar(255) NOT NULL,
+    [PasswordHash] nvarchar(255) NOT NULL,
+    [ExpiresOn] datetime2 NULL,
+    [MaximumAttempts] int NULL,
+    [AttemptCount] int NOT NULL,
+    [HasValidationSucceeded] bit NOT NULL,
+    [CustomAttributes] nvarchar(max) NULL,
+    [StreamId] nvarchar(255) NOT NULL,
+    [Version] bigint NOT NULL,
+    [CreatedBy] nvarchar(255) NULL,
+    [CreatedOn] datetime2 NOT NULL,
+    [UpdatedBy] nvarchar(255) NULL,
+    [UpdatedOn] datetime2 NOT NULL,
+    CONSTRAINT [PK_OneTimePasswords] PRIMARY KEY ([OneTimePasswordId])
+);
+
+CREATE TABLE [Identity].[Roles] (
+    [RoleId] int NOT NULL IDENTITY,
+    [TenantId] nvarchar(255) NULL,
+    [EntityId] nvarchar(255) NOT NULL,
+    [UniqueName] nvarchar(255) NOT NULL,
+    [UniqueNameNormalized] nvarchar(255) NOT NULL,
+    [DisplayName] nvarchar(255) NULL,
+    [Description] nvarchar(max) NULL,
+    [CustomAttributes] nvarchar(max) NULL,
+    [StreamId] nvarchar(255) NOT NULL,
+    [Version] bigint NOT NULL,
+    [CreatedBy] nvarchar(255) NULL,
+    [CreatedOn] datetime2 NOT NULL,
+    [UpdatedBy] nvarchar(255) NULL,
+    [UpdatedOn] datetime2 NOT NULL,
+    CONSTRAINT [PK_Roles] PRIMARY KEY ([RoleId])
+);
+
+CREATE TABLE [Identity].[TokenBlacklist] (
+    [BlacklistedTokenId] int NOT NULL IDENTITY,
+    [TokenId] nvarchar(255) NOT NULL,
+    [ExpiresOn] datetime2 NULL,
+    CONSTRAINT [PK_TokenBlacklist] PRIMARY KEY ([BlacklistedTokenId])
+);
+
+CREATE TABLE [Identity].[Users] (
+    [UserId] int NOT NULL IDENTITY,
+    [TenantId] nvarchar(255) NULL,
+    [EntityId] nvarchar(255) NOT NULL,
+    [UniqueName] nvarchar(255) NOT NULL,
+    [UniqueNameNormalized] nvarchar(255) NOT NULL,
+    [PasswordHash] nvarchar(255) NULL,
+    [PasswordChangedBy] nvarchar(255) NULL,
+    [PasswordChangedOn] datetime2 NULL,
+    [HasPassword] bit NOT NULL,
+    [DisabledBy] nvarchar(255) NULL,
+    [DisabledOn] datetime2 NULL,
+    [IsDisabled] bit NOT NULL,
+    [AddressStreet] nvarchar(255) NULL,
+    [AddressLocality] nvarchar(255) NULL,
+    [AddressPostalCode] nvarchar(255) NULL,
+    [AddressRegion] nvarchar(255) NULL,
+    [AddressCountry] nvarchar(255) NULL,
+    [AddressFormatted] nvarchar(1279) NULL,
+    [AddressVerifiedBy] nvarchar(255) NULL,
+    [AddressVerifiedOn] datetime2 NULL,
+    [IsAddressVerified] bit NOT NULL,
+    [EmailAddress] nvarchar(255) NULL,
+    [EmailAddressNormalized] nvarchar(255) NULL,
+    [EmailVerifiedBy] nvarchar(255) NULL,
+    [EmailVerifiedOn] datetime2 NULL,
+    [IsEmailVerified] bit NOT NULL,
+    [PhoneCountryCode] nvarchar(2) NULL,
+    [PhoneNumber] nvarchar(20) NULL,
+    [PhoneExtension] nvarchar(10) NULL,
+    [PhoneE164Formatted] nvarchar(40) NULL,
+    [PhoneVerifiedBy] nvarchar(255) NULL,
+    [PhoneVerifiedOn] datetime2 NULL,
+    [IsPhoneVerified] bit NOT NULL,
+    [IsConfirmed] bit NOT NULL,
+    [FirstName] nvarchar(255) NULL,
+    [MiddleName] nvarchar(255) NULL,
+    [LastName] nvarchar(255) NULL,
+    [FullName] nvarchar(767) NULL,
+    [Nickname] nvarchar(255) NULL,
+    [Birthdate] datetime2 NULL,
+    [Gender] nvarchar(255) NULL,
+    [Locale] nvarchar(16) NULL,
+    [TimeZone] nvarchar(32) NULL,
+    [Picture] nvarchar(2048) NULL,
+    [Profile] nvarchar(2048) NULL,
+    [Website] nvarchar(2048) NULL,
+    [AuthenticatedOn] datetime2 NULL,
+    [CustomAttributes] nvarchar(max) NULL,
+    [StreamId] nvarchar(255) NOT NULL,
+    [Version] bigint NOT NULL,
+    [CreatedBy] nvarchar(255) NULL,
+    [CreatedOn] datetime2 NOT NULL,
+    [UpdatedBy] nvarchar(255) NULL,
+    [UpdatedOn] datetime2 NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY ([UserId])
+);
+
+CREATE TABLE [Identity].[ApiKeyRoles] (
+    [ApiKeyId] int NOT NULL,
+    [RoleId] int NOT NULL,
+    CONSTRAINT [PK_ApiKeyRoles] PRIMARY KEY ([ApiKeyId], [RoleId]),
+    CONSTRAINT [FK_ApiKeyRoles_ApiKeys_ApiKeyId] FOREIGN KEY ([ApiKeyId]) REFERENCES [Identity].[ApiKeys] ([ApiKeyId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_ApiKeyRoles_Roles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [Identity].[Roles] ([RoleId]) ON DELETE CASCADE
+);
+
+CREATE TABLE [Identity].[Sessions] (
+    [SessionId] int NOT NULL IDENTITY,
+    [TenantId] nvarchar(255) NULL,
+    [EntityId] nvarchar(255) NOT NULL,
+    [UserId] int NOT NULL,
+    [SecretHash] nvarchar(255) NULL,
+    [IsPersistent] bit NOT NULL,
+    [SignedOutBy] nvarchar(255) NULL,
+    [SignedOutOn] datetime2 NULL,
+    [IsActive] bit NOT NULL,
+    [CustomAttributes] nvarchar(max) NULL,
+    [StreamId] nvarchar(255) NOT NULL,
+    [Version] bigint NOT NULL,
+    [CreatedBy] nvarchar(255) NULL,
+    [CreatedOn] datetime2 NOT NULL,
+    [UpdatedBy] nvarchar(255) NULL,
+    [UpdatedOn] datetime2 NOT NULL,
+    CONSTRAINT [PK_Sessions] PRIMARY KEY ([SessionId]),
+    CONSTRAINT [FK_Sessions_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Identity].[Users] ([UserId]) ON DELETE NO ACTION
+);
+
+CREATE TABLE [Identity].[UserIdentifiers] (
+    [UserIdentifierId] int NOT NULL IDENTITY,
+    [UserId] int NOT NULL,
+    [TenantId] nvarchar(255) NULL,
+    [Key] nvarchar(255) NOT NULL,
+    [Value] nvarchar(255) NOT NULL,
+    CONSTRAINT [PK_UserIdentifiers] PRIMARY KEY ([UserIdentifierId]),
+    CONSTRAINT [FK_UserIdentifiers_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Identity].[Users] ([UserId]) ON DELETE CASCADE
+);
+
+CREATE TABLE [Identity].[UserRoles] (
+    [UserId] int NOT NULL,
+    [RoleId] int NOT NULL,
+    CONSTRAINT [PK_UserRoles] PRIMARY KEY ([UserId], [RoleId]),
+    CONSTRAINT [FK_UserRoles_Roles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [Identity].[Roles] ([RoleId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_UserRoles_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Identity].[Users] ([UserId]) ON DELETE CASCADE
+);
+
+CREATE INDEX [IX_Actors_DisplayName] ON [Identity].[Actors] ([DisplayName]);
+
+CREATE INDEX [IX_Actors_EmailAddress] ON [Identity].[Actors] ([EmailAddress]);
+
+CREATE UNIQUE INDEX [IX_Actors_Id] ON [Identity].[Actors] ([Id]);
+
+CREATE INDEX [IX_Actors_IsDeleted] ON [Identity].[Actors] ([IsDeleted]);
+
+CREATE INDEX [IX_Actors_Type] ON [Identity].[Actors] ([Type]);
+
+CREATE INDEX [IX_ApiKeyRoles_RoleId] ON [Identity].[ApiKeyRoles] ([RoleId]);
+
+CREATE INDEX [IX_ApiKeys_AuthenticatedOn] ON [Identity].[ApiKeys] ([AuthenticatedOn]);
+
+CREATE INDEX [IX_ApiKeys_CreatedBy] ON [Identity].[ApiKeys] ([CreatedBy]);
+
+CREATE INDEX [IX_ApiKeys_CreatedOn] ON [Identity].[ApiKeys] ([CreatedOn]);
+
+CREATE INDEX [IX_ApiKeys_DisplayName] ON [Identity].[ApiKeys] ([DisplayName]);
+
+CREATE INDEX [IX_ApiKeys_EntityId] ON [Identity].[ApiKeys] ([EntityId]);
+
+CREATE INDEX [IX_ApiKeys_ExpiresOn] ON [Identity].[ApiKeys] ([ExpiresOn]);
+
+CREATE UNIQUE INDEX [IX_ApiKeys_StreamId] ON [Identity].[ApiKeys] ([StreamId]);
+
+CREATE UNIQUE INDEX [IX_ApiKeys_TenantId_EntityId] ON [Identity].[ApiKeys] ([TenantId], [EntityId]) WHERE [TenantId] IS NOT NULL;
+
+CREATE INDEX [IX_ApiKeys_UpdatedBy] ON [Identity].[ApiKeys] ([UpdatedBy]);
+
+CREATE INDEX [IX_ApiKeys_UpdatedOn] ON [Identity].[ApiKeys] ([UpdatedOn]);
+
+CREATE INDEX [IX_ApiKeys_Version] ON [Identity].[ApiKeys] ([Version]);
+
+CREATE INDEX [IX_CustomAttributes_EntityType_EntityId] ON [Identity].[CustomAttributes] ([EntityType], [EntityId]);
+
+CREATE UNIQUE INDEX [IX_CustomAttributes_EntityType_EntityId_Key] ON [Identity].[CustomAttributes] ([EntityType], [EntityId], [Key]);
+
+CREATE INDEX [IX_CustomAttributes_Key] ON [Identity].[CustomAttributes] ([Key]);
+
+CREATE INDEX [IX_CustomAttributes_ValueShortened] ON [Identity].[CustomAttributes] ([ValueShortened]);
+
+CREATE INDEX [IX_OneTimePasswords_AttemptCount] ON [Identity].[OneTimePasswords] ([AttemptCount]);
+
+CREATE INDEX [IX_OneTimePasswords_CreatedBy] ON [Identity].[OneTimePasswords] ([CreatedBy]);
+
+CREATE INDEX [IX_OneTimePasswords_CreatedOn] ON [Identity].[OneTimePasswords] ([CreatedOn]);
+
+CREATE INDEX [IX_OneTimePasswords_EntityId] ON [Identity].[OneTimePasswords] ([EntityId]);
+
+CREATE INDEX [IX_OneTimePasswords_ExpiresOn] ON [Identity].[OneTimePasswords] ([ExpiresOn]);
+
+CREATE INDEX [IX_OneTimePasswords_HasValidationSucceeded] ON [Identity].[OneTimePasswords] ([HasValidationSucceeded]);
+
+CREATE INDEX [IX_OneTimePasswords_MaximumAttempts] ON [Identity].[OneTimePasswords] ([MaximumAttempts]);
+
+CREATE UNIQUE INDEX [IX_OneTimePasswords_StreamId] ON [Identity].[OneTimePasswords] ([StreamId]);
+
+CREATE UNIQUE INDEX [IX_OneTimePasswords_TenantId_EntityId] ON [Identity].[OneTimePasswords] ([TenantId], [EntityId]) WHERE [TenantId] IS NOT NULL;
+
+CREATE INDEX [IX_OneTimePasswords_UpdatedBy] ON [Identity].[OneTimePasswords] ([UpdatedBy]);
+
+CREATE INDEX [IX_OneTimePasswords_UpdatedOn] ON [Identity].[OneTimePasswords] ([UpdatedOn]);
+
+CREATE INDEX [IX_OneTimePasswords_Version] ON [Identity].[OneTimePasswords] ([Version]);
+
+CREATE INDEX [IX_Roles_CreatedBy] ON [Identity].[Roles] ([CreatedBy]);
+
+CREATE INDEX [IX_Roles_CreatedOn] ON [Identity].[Roles] ([CreatedOn]);
+
+CREATE INDEX [IX_Roles_DisplayName] ON [Identity].[Roles] ([DisplayName]);
+
+CREATE INDEX [IX_Roles_EntityId] ON [Identity].[Roles] ([EntityId]);
+
+CREATE UNIQUE INDEX [IX_Roles_StreamId] ON [Identity].[Roles] ([StreamId]);
+
+CREATE UNIQUE INDEX [IX_Roles_TenantId_EntityId] ON [Identity].[Roles] ([TenantId], [EntityId]) WHERE [TenantId] IS NOT NULL;
+
+CREATE UNIQUE INDEX [IX_Roles_TenantId_UniqueNameNormalized] ON [Identity].[Roles] ([TenantId], [UniqueNameNormalized]) WHERE [TenantId] IS NOT NULL;
+
+CREATE INDEX [IX_Roles_UniqueName] ON [Identity].[Roles] ([UniqueName]);
+
+CREATE INDEX [IX_Roles_UpdatedBy] ON [Identity].[Roles] ([UpdatedBy]);
+
+CREATE INDEX [IX_Roles_UpdatedOn] ON [Identity].[Roles] ([UpdatedOn]);
+
+CREATE INDEX [IX_Roles_Version] ON [Identity].[Roles] ([Version]);
+
+CREATE INDEX [IX_Sessions_CreatedBy] ON [Identity].[Sessions] ([CreatedBy]);
+
+CREATE INDEX [IX_Sessions_CreatedOn] ON [Identity].[Sessions] ([CreatedOn]);
+
+CREATE INDEX [IX_Sessions_EntityId] ON [Identity].[Sessions] ([EntityId]);
+
+CREATE INDEX [IX_Sessions_IsActive] ON [Identity].[Sessions] ([IsActive]);
+
+CREATE INDEX [IX_Sessions_IsPersistent] ON [Identity].[Sessions] ([IsPersistent]);
+
+CREATE INDEX [IX_Sessions_SignedOutBy] ON [Identity].[Sessions] ([SignedOutBy]);
+
+CREATE INDEX [IX_Sessions_SignedOutOn] ON [Identity].[Sessions] ([SignedOutOn]);
+
+CREATE UNIQUE INDEX [IX_Sessions_StreamId] ON [Identity].[Sessions] ([StreamId]);
+
+CREATE UNIQUE INDEX [IX_Sessions_TenantId_EntityId] ON [Identity].[Sessions] ([TenantId], [EntityId]) WHERE [TenantId] IS NOT NULL;
+
+CREATE INDEX [IX_Sessions_UpdatedBy] ON [Identity].[Sessions] ([UpdatedBy]);
+
+CREATE INDEX [IX_Sessions_UpdatedOn] ON [Identity].[Sessions] ([UpdatedOn]);
+
+CREATE INDEX [IX_Sessions_UserId] ON [Identity].[Sessions] ([UserId]);
+
+CREATE INDEX [IX_Sessions_Version] ON [Identity].[Sessions] ([Version]);
+
+CREATE INDEX [IX_TokenBlacklist_ExpiresOn] ON [Identity].[TokenBlacklist] ([ExpiresOn]);
+
+CREATE UNIQUE INDEX [IX_TokenBlacklist_TokenId] ON [Identity].[TokenBlacklist] ([TokenId]);
+
+CREATE INDEX [IX_UserIdentifiers_Key] ON [Identity].[UserIdentifiers] ([Key]);
+
+CREATE UNIQUE INDEX [IX_UserIdentifiers_TenantId_Key_Value] ON [Identity].[UserIdentifiers] ([TenantId], [Key], [Value]) WHERE [TenantId] IS NOT NULL;
+
+CREATE UNIQUE INDEX [IX_UserIdentifiers_UserId_Key] ON [Identity].[UserIdentifiers] ([UserId], [Key]);
+
+CREATE INDEX [IX_UserIdentifiers_Value] ON [Identity].[UserIdentifiers] ([Value]);
+
+CREATE INDEX [IX_UserRoles_RoleId] ON [Identity].[UserRoles] ([RoleId]);
+
+CREATE INDEX [IX_Users_AddressCountry] ON [Identity].[Users] ([AddressCountry]);
+
+CREATE INDEX [IX_Users_AddressFormatted] ON [Identity].[Users] ([AddressFormatted]);
+
+CREATE INDEX [IX_Users_AddressLocality] ON [Identity].[Users] ([AddressLocality]);
+
+CREATE INDEX [IX_Users_AddressPostalCode] ON [Identity].[Users] ([AddressPostalCode]);
+
+CREATE INDEX [IX_Users_AddressRegion] ON [Identity].[Users] ([AddressRegion]);
+
+CREATE INDEX [IX_Users_AddressStreet] ON [Identity].[Users] ([AddressStreet]);
+
+CREATE INDEX [IX_Users_AddressVerifiedBy] ON [Identity].[Users] ([AddressVerifiedBy]);
+
+CREATE INDEX [IX_Users_AddressVerifiedOn] ON [Identity].[Users] ([AddressVerifiedOn]);
+
+CREATE INDEX [IX_Users_AuthenticatedOn] ON [Identity].[Users] ([AuthenticatedOn]);
+
+CREATE INDEX [IX_Users_Birthdate] ON [Identity].[Users] ([Birthdate]);
+
+CREATE INDEX [IX_Users_CreatedBy] ON [Identity].[Users] ([CreatedBy]);
+
+CREATE INDEX [IX_Users_CreatedOn] ON [Identity].[Users] ([CreatedOn]);
+
+CREATE INDEX [IX_Users_DisabledBy] ON [Identity].[Users] ([DisabledBy]);
+
+CREATE INDEX [IX_Users_DisabledOn] ON [Identity].[Users] ([DisabledOn]);
+
+CREATE INDEX [IX_Users_EmailAddress] ON [Identity].[Users] ([EmailAddress]);
+
+CREATE INDEX [IX_Users_EmailVerifiedBy] ON [Identity].[Users] ([EmailVerifiedBy]);
+
+CREATE INDEX [IX_Users_EmailVerifiedOn] ON [Identity].[Users] ([EmailVerifiedOn]);
+
+CREATE INDEX [IX_Users_EntityId] ON [Identity].[Users] ([EntityId]);
+
+CREATE INDEX [IX_Users_FirstName] ON [Identity].[Users] ([FirstName]);
+
+CREATE INDEX [IX_Users_FullName] ON [Identity].[Users] ([FullName]);
+
+CREATE INDEX [IX_Users_Gender] ON [Identity].[Users] ([Gender]);
+
+CREATE INDEX [IX_Users_HasPassword] ON [Identity].[Users] ([HasPassword]);
+
+CREATE INDEX [IX_Users_IsAddressVerified] ON [Identity].[Users] ([IsAddressVerified]);
+
+CREATE INDEX [IX_Users_IsConfirmed] ON [Identity].[Users] ([IsConfirmed]);
+
+CREATE INDEX [IX_Users_IsDisabled] ON [Identity].[Users] ([IsDisabled]);
+
+CREATE INDEX [IX_Users_IsEmailVerified] ON [Identity].[Users] ([IsEmailVerified]);
+
+CREATE INDEX [IX_Users_IsPhoneVerified] ON [Identity].[Users] ([IsPhoneVerified]);
+
+CREATE INDEX [IX_Users_LastName] ON [Identity].[Users] ([LastName]);
+
+CREATE INDEX [IX_Users_Locale] ON [Identity].[Users] ([Locale]);
+
+CREATE INDEX [IX_Users_MiddleName] ON [Identity].[Users] ([MiddleName]);
+
+CREATE INDEX [IX_Users_Nickname] ON [Identity].[Users] ([Nickname]);
+
+CREATE INDEX [IX_Users_PasswordChangedBy] ON [Identity].[Users] ([PasswordChangedBy]);
+
+CREATE INDEX [IX_Users_PasswordChangedOn] ON [Identity].[Users] ([PasswordChangedOn]);
+
+CREATE INDEX [IX_Users_PhoneCountryCode] ON [Identity].[Users] ([PhoneCountryCode]);
+
+CREATE INDEX [IX_Users_PhoneE164Formatted] ON [Identity].[Users] ([PhoneE164Formatted]);
+
+CREATE INDEX [IX_Users_PhoneExtension] ON [Identity].[Users] ([PhoneExtension]);
+
+CREATE INDEX [IX_Users_PhoneNumber] ON [Identity].[Users] ([PhoneNumber]);
+
+CREATE INDEX [IX_Users_PhoneVerifiedBy] ON [Identity].[Users] ([PhoneVerifiedBy]);
+
+CREATE INDEX [IX_Users_PhoneVerifiedOn] ON [Identity].[Users] ([PhoneVerifiedOn]);
+
+CREATE UNIQUE INDEX [IX_Users_StreamId] ON [Identity].[Users] ([StreamId]);
+
+CREATE INDEX [IX_Users_TenantId_EmailAddressNormalized] ON [Identity].[Users] ([TenantId], [EmailAddressNormalized]);
+
+CREATE UNIQUE INDEX [IX_Users_TenantId_EntityId] ON [Identity].[Users] ([TenantId], [EntityId]) WHERE [TenantId] IS NOT NULL;
+
+CREATE UNIQUE INDEX [IX_Users_TenantId_UniqueNameNormalized] ON [Identity].[Users] ([TenantId], [UniqueNameNormalized]) WHERE [TenantId] IS NOT NULL;
+
+CREATE INDEX [IX_Users_TimeZone] ON [Identity].[Users] ([TimeZone]);
+
+CREATE INDEX [IX_Users_UniqueName] ON [Identity].[Users] ([UniqueName]);
+
+CREATE INDEX [IX_Users_UpdatedBy] ON [Identity].[Users] ([UpdatedBy]);
+
+CREATE INDEX [IX_Users_UpdatedOn] ON [Identity].[Users] ([UpdatedOn]);
+
+CREATE INDEX [IX_Users_Version] ON [Identity].[Users] ([Version]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20241223050308_Release_3_0_0', N'9.0.0');
+
+COMMIT;
+GO
---
+++ tests/Logitar.Identity.IntegrationTests/EntityFrameworkCore/SqlServer/OneTimePasswordSqlServerIntegrationTests.cs
@@ -0,0 +1,217 @@
+﻿using Logitar.EventSourcing;
+using Logitar.Identity.Core;
+using Logitar.Identity.Core.Passwords;
+using Logitar.Identity.EntityFrameworkCore.Relational;
+using Logitar.Identity.EntityFrameworkCore.Relational.Entities;
+using Logitar.Identity.Infrastructure.Passwords.Pbkdf2;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Logitar.Identity.EntityFrameworkCore.SqlServer;
+
+[Trait(Traits.Category, Categories.Integration)]
+public class OneTimePasswordSqlServerIntegrationTests : IntegrationTests
+{
+  private const string PasswordString = "671359";
+
+  private readonly IOneTimePasswordRepository _oneTimePasswordRepository;
+
+  private readonly ActorId _actorId = ActorId.NewId();
+  private readonly Password _password;
+
+  public OneTimePasswordSqlServerIntegrationTests() : base(DatabaseProvider.SqlServer)
+  {
+    _oneTimePasswordRepository = ServiceProvider.GetRequiredService<IOneTimePasswordRepository>();
+
+    Pbkdf2Settings pbkdf2 = new();
+    _password = new Pbkdf2Password(PasswordString, pbkdf2.Algorithm, pbkdf2.Iterations, pbkdf2.SaltLength, pbkdf2.HashLength);
+  }
+
+  [Theory(DisplayName = "LoadAsync: it should return the correct result, given a deletion status.")]
+  [InlineData(null)]
+  [InlineData(false)]
+  [InlineData(true)]
+  public async Task Given_Deleted_When_LoadAsync_Then_CorrectResult(bool? isDeleted)
+  {
+    OneTimePassword oneTimePassword1 = new(_password);
+    OneTimePassword oneTimePassword2 = new(_password);
+    oneTimePassword2.Delete();
+    OneTimePassword oneTimePassword3 = new(_password);
+    if (isDeleted == true)
+    {
+      oneTimePassword3.Delete();
+    }
+    if (isDeleted.HasValue)
+    {
+      await _oneTimePasswordRepository.SaveAsync([oneTimePassword1, oneTimePassword2, oneTimePassword3]);
+    }
+
+    IReadOnlyCollection<OneTimePassword> oneTimePasswords = await _oneTimePasswordRepository.LoadAsync(isDeleted);
+
+    if (isDeleted.HasValue)
+    {
+      Assert.Equal(2, oneTimePasswords.Count);
+      Assert.Contains(oneTimePassword3, oneTimePasswords);
+      Assert.Contains(isDeleted.Value ? oneTimePassword2 : oneTimePassword1, oneTimePasswords);
+    }
+    else
+    {
+      Assert.Empty(oneTimePasswords);
+    }
+  }
+
+  [Theory(DisplayName = "LoadAsync: it should return the correct result, given IDs and a deletion status.")]
+  [InlineData(null)]
+  [InlineData(false)]
+  [InlineData(true)]
+  public async Task Given_IdsDeleted_When_LoadAsync_Then_CorrectResult(bool? isDeleted)
+  {
+    OneTimePassword oneTimePassword1 = new(_password);
+    OneTimePassword oneTimePassword2 = new(_password);
+    oneTimePassword2.Delete();
+    OneTimePassword oneTimePassword3 = new(_password);
+    if (isDeleted == true)
+    {
+      oneTimePassword3.Delete();
+    }
+    OneTimePassword oneTimePassword4 = new(_password);
+    if (isDeleted == true)
+    {
+      oneTimePassword4.Delete();
+    }
+    if (isDeleted.HasValue)
+    {
+      await _oneTimePasswordRepository.SaveAsync([oneTimePassword1, oneTimePassword2, oneTimePassword3, oneTimePassword4]);
+    }
+
+    IReadOnlyCollection<OneTimePassword> oneTimePasswords = await _oneTimePasswordRepository.LoadAsync([oneTimePassword1.Id, oneTimePassword2.Id, oneTimePassword3.Id, OneTimePasswordId.NewId()], isDeleted);
+
+    if (isDeleted.HasValue)
+    {
+      Assert.Equal(2, oneTimePasswords.Count);
+      Assert.Contains(oneTimePassword3, oneTimePasswords);
+      Assert.Contains(isDeleted.Value ? oneTimePassword2 : oneTimePassword1, oneTimePasswords);
+    }
+    else
+    {
+      Assert.Empty(oneTimePasswords);
+    }
+  }
+
+  [Theory(DisplayName = "LoadAsync: it should return the correct result, given an ID, a version and a deletion status.")]
+  [InlineData(false, null, false)]
+  [InlineData(true, null, false)]
+  [InlineData(true, false, false)]
+  [InlineData(true, true, true)]
+  public async Task Given_IdVersionDeleted_When_LoadAsync_Then_CorrectResult(bool found, bool? isDeleted, bool withVersion)
+  {
+    OneTimePassword oneTimePassword = new(_password);
+    if (isDeleted == true)
+    {
+      oneTimePassword.Delete();
+    }
+    if (found)
+    {
+      await _oneTimePasswordRepository.SaveAsync(oneTimePassword);
+    }
+
+    long? version = withVersion ? 1 : null;
+    OneTimePassword? result = await _oneTimePasswordRepository.LoadAsync(oneTimePassword.Id, version, isDeleted);
+    if (found)
+    {
+      Assert.NotNull(result);
+      if (withVersion)
+      {
+        Assert.Equal(version, result.Version);
+        Assert.False(result.IsDeleted);
+      }
+      else
+      {
+        Assert.Equal(oneTimePassword.Version, result.Version);
+        Assert.Equal(isDeleted ?? false, result.IsDeleted);
+      }
+    }
+    else
+    {
+      Assert.Null(result);
+    }
+  }
+
+  [Theory(DisplayName = "LoadAsync: it should return the correct result, given a tenant ID.")]
+  [InlineData(false, null)]
+  [InlineData(true, null)]
+  [InlineData(true, "9699e381-3965-4d77-ada2-e0b3742c3b91")]
+  public async Task Given_TenantId_When_LoadAsync_Then_CorrectResults(bool found, string? tenantIdValue)
+  {
+    OneTimePassword oneTimePassword1 = new(_password);
+    TenantId tenantId = tenantIdValue == null ? TenantId.NewId() : new(tenantIdValue);
+    OneTimePassword oneTimePassword2 = new(_password, expiresOn: null, maximumAttempts: null, actorId: null, OneTimePasswordId.NewId(tenantId));
+    if (found)
+    {
+      await _oneTimePasswordRepository.SaveAsync([oneTimePassword1, oneTimePassword2]);
+    }
+
+    IReadOnlyCollection<OneTimePassword> oneTimePasswords = await _oneTimePasswordRepository.LoadAsync(tenantIdValue == null ? null : tenantId);
+
+    if (found)
+    {
+      Assert.Equal(tenantIdValue == null ? oneTimePassword1 : oneTimePassword2, Assert.Single(oneTimePasswords));
+    }
+    else
+    {
+      Assert.Empty(oneTimePasswords);
+    }
+  }
+
+  [Fact(DisplayName = "SaveAsync: it should save the oneTimePassword correctly.")]
+  public async Task Given_OneTimePassword_When_SaveAsync_Then_SavedCorrectly()
+  {
+    TenantId tenantId = TenantId.NewId();
+
+    DateTime expiresOn = DateTime.Now.AddHours(1);
+    int maximumAttempts = 5;
+    OneTimePassword oneTimePassword = new(_password, expiresOn, maximumAttempts, _actorId, OneTimePasswordId.NewId(tenantId));
+    Assert.Throws<IncorrectOneTimePasswordPasswordException>(() => oneTimePassword.Validate("invalid"));
+    oneTimePassword.Validate(PasswordString, _actorId);
+    string userId = Guid.NewGuid().ToString();
+    oneTimePassword.SetCustomAttribute(new Identifier("UserId"), userId);
+    oneTimePassword.Update();
+    await _oneTimePasswordRepository.SaveAsync(oneTimePassword);
+
+    OneTimePasswordEntity? entity = await IdentityContext.OneTimePasswords.AsNoTracking().SingleOrDefaultAsync();
+
+    Assert.NotNull(entity);
+    Assert.Equal(oneTimePassword.Id.Value, entity.StreamId);
+    Assert.Equal(oneTimePassword.Version, entity.Version);
+    Assert.Equal(oneTimePassword.CreatedBy?.Value, entity.CreatedBy);
+    Assert.Equal(oneTimePassword.CreatedOn.AsUniversalTime(), entity.CreatedOn, TimeSpan.FromSeconds(1));
+    Assert.Equal(oneTimePassword.UpdatedBy?.Value, entity.UpdatedBy);
+    Assert.Equal(oneTimePassword.UpdatedOn.AsUniversalTime(), entity.UpdatedOn, TimeSpan.FromSeconds(1));
+    Assert.Equal(tenantId.Value, entity.TenantId);
+    Assert.Equal(oneTimePassword.EntityId.Value, entity.EntityId);
+    Assert.Equal(_password.Encode(), entity.PasswordHash);
+    Assert.True(entity.ExpiresOn.HasValue);
+    Assert.Equal(expiresOn.AsUniversalTime(), entity.ExpiresOn.Value, TimeSpan.FromSeconds(1));
+    Assert.Equal(maximumAttempts, entity.MaximumAttempts);
+    Assert.Equal(2, entity.AttemptCount);
+    Assert.True(entity.HasValidationSucceeded);
+    Assert.Equal($@"{{""UserId"":""{userId}""}}", entity.CustomAttributes);
+
+    CustomAttributeEntity[] customAttributes = await IdentityContext.CustomAttributes.AsNoTracking().ToArrayAsync();
+    Assert.Equal(oneTimePassword.CustomAttributes.Count, customAttributes.Length);
+    foreach (KeyValuePair<Identifier, string> customAttribute in oneTimePassword.CustomAttributes)
+    {
+      Assert.Contains(customAttributes, c => c.EntityType == EntityType.OneTimePassword && c.EntityId == entity.OneTimePasswordId && c.Key == customAttribute.Key.Value
+        && c.Value == customAttribute.Value && c.ValueShortened == customAttribute.Value.Truncate(byte.MaxValue));
+    }
+
+    oneTimePassword.Delete();
+    await _oneTimePasswordRepository.SaveAsync(oneTimePassword);
+
+    entity = await IdentityContext.OneTimePasswords.AsNoTracking().SingleOrDefaultAsync();
+    Assert.Null(entity);
+
+    customAttributes = await IdentityContext.CustomAttributes.AsNoTracking().ToArrayAsync();
+    Assert.Empty(customAttributes);
+  }
+}
