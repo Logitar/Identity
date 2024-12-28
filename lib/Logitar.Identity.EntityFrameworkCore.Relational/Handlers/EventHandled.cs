6d807b8b212b19e961d276f505a27fd7af2dd1cf
Francis Pion <francispion@hotmail.com>
2024-12-27 20:39:26 -0500

    Event handlers are now idempotent. (#91)

---
+++ lib/Logitar.Identity.EntityFrameworkCore.Relational/Handlers/EventHandled.cs
@@ -0,0 +1,6 @@
+﻿using Logitar.EventSourcing;
+using MediatR;
+
+namespace Logitar.Identity.EntityFrameworkCore.Relational.Handlers;
+
+public record EventHandled(DomainEvent Event) : INotification;
--- lib/Logitar.Identity.EntityFrameworkCore.Relational/Handlers/OneTimePasswordEvents.cs
+++ lib/Logitar.Identity.EntityFrameworkCore.Relational/Handlers/OneTimePasswordEvents.cs
@@ -13,17 +13,20 @@ public sealed class OneTimePasswordEvents : INotificationHandler<OneTimePassword
 {
   private readonly IdentityContext _context;
   private readonly ICustomAttributeService _customAttributes;
+  private readonly IMediator _mediator;
 
-  public OneTimePasswordEvents(IdentityContext context, ICustomAttributeService customAttributes)
+  public OneTimePasswordEvents(IdentityContext context, ICustomAttributeService customAttributes, IMediator mediator)
   {
     _context = context;
     _customAttributes = customAttributes;
+    _mediator = mediator;
   }
 
   public async Task Handle(OneTimePasswordCreated @event, CancellationToken cancellationToken)
   {
     OneTimePasswordEntity? oneTimePassword = await _context.OneTimePasswords.AsNoTracking()
       .SingleOrDefaultAsync(x => x.StreamId == @event.StreamId.Value, cancellationToken);
+
     if (oneTimePassword == null)
     {
       oneTimePassword = new(@event);
@@ -31,6 +34,12 @@ public sealed class OneTimePasswordEvents : INotificationHandler<OneTimePassword
       _context.OneTimePasswords.Add(oneTimePassword);
 
       await _context.SaveChangesAsync(cancellationToken);
+
+      await _mediator.Publish(new EventHandled(@event), cancellationToken);
+    }
+    else
+    {
+      await _mediator.Publish(new EventNotHandled(@event, oneTimePassword), cancellationToken);
     }
   }
 
@@ -38,46 +47,77 @@ public sealed class OneTimePasswordEvents : INotificationHandler<OneTimePassword
   {
     OneTimePasswordEntity? oneTimePassword = await _context.OneTimePasswords
       .SingleOrDefaultAsync(x => x.StreamId == @event.StreamId.Value, cancellationToken);
-    if (oneTimePassword != null)
+
+    if (oneTimePassword == null)
+    {
+      await _mediator.Publish(new EventNotHandled(@event, oneTimePassword), cancellationToken);
+    }
+    else
     {
       _context.OneTimePasswords.Remove(oneTimePassword);
 
       await _customAttributes.RemoveAsync(EntityType.OneTimePassword, oneTimePassword.OneTimePasswordId, cancellationToken);
       await _context.SaveChangesAsync(cancellationToken);
+
+      await _mediator.Publish(new EventHandled(@event), cancellationToken);
     }
   }
 
   public async Task Handle(OneTimePasswordUpdated @event, CancellationToken cancellationToken)
   {
-    OneTimePasswordEntity oneTimePassword = await _context.OneTimePasswords
-      .SingleOrDefaultAsync(x => x.StreamId == @event.StreamId.Value, cancellationToken)
-      ?? throw new InvalidOperationException($"The One-Time Password (OTP) entity 'StreamId={@event.StreamId}' could not be found.");
+    OneTimePasswordEntity? oneTimePassword = await _context.OneTimePasswords
+      .SingleOrDefaultAsync(x => x.StreamId == @event.StreamId.Value, cancellationToken);
 
-    oneTimePassword.Update(@event);
+    if (oneTimePassword == null || oneTimePassword.Version != (@event.Version - 1))
+    {
+      await _mediator.Publish(new EventNotHandled(@event, oneTimePassword), cancellationToken);
+    }
+    else
+    {
+      oneTimePassword.Update(@event);
 
-    await _customAttributes.UpdateAsync(EntityType.OneTimePassword, oneTimePassword.OneTimePasswordId, @event.CustomAttributes, cancellationToken);
-    await _context.SaveChangesAsync(cancellationToken);
+      await _customAttributes.UpdateAsync(EntityType.OneTimePassword, oneTimePassword.OneTimePasswordId, @event.CustomAttributes, cancellationToken);
+      await _context.SaveChangesAsync(cancellationToken);
+
+      await _mediator.Publish(new EventHandled(@event), cancellationToken);
+    }
   }
 
   public async Task Handle(OneTimePasswordValidationFailed @event, CancellationToken cancellationToken)
   {
-    OneTimePasswordEntity oneTimePassword = await _context.OneTimePasswords
-      .SingleOrDefaultAsync(x => x.StreamId == @event.StreamId.Value, cancellationToken)
-      ?? throw new InvalidOperationException($"The One-Time Password (OTP) entity 'StreamId={@event.StreamId}' could not be found.");
+    OneTimePasswordEntity? oneTimePassword = await _context.OneTimePasswords
+      .SingleOrDefaultAsync(x => x.StreamId == @event.StreamId.Value, cancellationToken);
 
-    oneTimePassword.Fail(@event);
+    if (oneTimePassword == null || oneTimePassword.Version != (@event.Version - 1))
+    {
+      await _mediator.Publish(new EventNotHandled(@event, oneTimePassword), cancellationToken);
+    }
+    else
+    {
+      oneTimePassword.Fail(@event);
 
-    await _context.SaveChangesAsync(cancellationToken);
+      await _context.SaveChangesAsync(cancellationToken);
+
+      await _mediator.Publish(new EventHandled(@event), cancellationToken);
+    }
   }
 
   public async Task Handle(OneTimePasswordValidationSucceeded @event, CancellationToken cancellationToken)
   {
-    OneTimePasswordEntity oneTimePassword = await _context.OneTimePasswords
-      .SingleOrDefaultAsync(x => x.StreamId == @event.StreamId.Value, cancellationToken)
-      ?? throw new InvalidOperationException($"The One-Time Password (OTP) entity 'StreamId={@event.StreamId}' could not be found.");
+    OneTimePasswordEntity? oneTimePassword = await _context.OneTimePasswords
+      .SingleOrDefaultAsync(x => x.StreamId == @event.StreamId.Value, cancellationToken);
 
-    oneTimePassword.Succeed(@event);
+    if (oneTimePassword == null || oneTimePassword.Version != (@event.Version - 1))
+    {
+      await _mediator.Publish(new EventNotHandled(@event, oneTimePassword), cancellationToken);
+    }
+    else
+    {
+      oneTimePassword.Succeed(@event);
 
-    await _context.SaveChangesAsync(cancellationToken);
+      await _context.SaveChangesAsync(cancellationToken);
+
+      await _mediator.Publish(new EventHandled(@event), cancellationToken);
+    }
   }
 }
